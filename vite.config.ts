a1353d8ba27abd35e23bf364ac5ecef0104f7bc3
KanekiKen <vkanekisenpai@gmail.com>
2022-12-05 21:48:30 +0100

    FSA-75 test

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5137,
+  },
   plugins: [react()],
   test: {
     globals: true,
