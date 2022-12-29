cf8a17abee4c97fa80241122f3633c2c2236a4b2
Kasper Mróz <kaspermroz1234@gmail.com>
2022-12-29 16:26:42 +0100

    [FSA-25] Implement all cinemas view, configure SVGs and TS aliases (#8)

--- src/views/AllCinemas/AllCinemas.tsx
+++ src/views/AllCinemas/AllCinemas.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from "react-query";
-import { Cinema } from "../../models/Cinema";
-import { getCinemas } from "../../services/CinemaService";
+import { Cinema } from "~/models/Cinema";
+import { getCinemas } from "~/services/CinemaService";
 import { CinemaCard } from "./CinemaCard";
+import { ReactComponent as ArrowDownIcon } from "~/assets/icons/ArrowDown.svg";
 
 export const AllCinemas = () => {
   const { data, isLoading, isFetched, isError } = useQuery<Cinema[]>(
@@ -10,13 +11,27 @@ export const AllCinemas = () => {
   );
 
   return (
-    <div>
+    <div className="flex flex-col justify-center align-baseline">
+      <h1 className="text-2xl md:text-5xl lg:text-7xl text-center mb-6 lg:mb-12 font-bold">
+        Explore cinemas near you
+      </h1>
       {isLoading ? <p>Loading..</p> : null}
       {isFetched ? (
         <>
-          {data?.map(({ id, name, address }) => (
-            <CinemaCard key={id} name={name} url={address} />
-          ))}
+          <div className="flex justify-center md:mb-8 lg:mb-10">
+            <div className="flex flex-col align-center md:flex-row">
+              {data?.map(({ id, name, address }) => (
+                <CinemaCard key={id} name={name} url={address} />
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-row px-4">
+            <button>
+              <ArrowDownIcon className="inline-block align-baseline mr-2" />
+              {/*TODO use theme color `link` */}
+              <span className="text-[#566CDF] text-2xl">See More</span>
+            </button>
+          </div>
         </>
       ) : null}
       {isError ? <p>An error ocurred!</p> : null}
--- src/views/AllCinemas/CinemaCard.tsx
+++ src/views/AllCinemas/CinemaCard.tsx
@@ -5,10 +5,42 @@ type CinemaCardProps = {
 
 export const CinemaCard = ({ name, url }: CinemaCardProps) => {
   return (
-    <a href={url} target="_blank" rel="noreferrer">
-      <div>
-        <p>{name}</p>
-      </div>
+    <a
+      href={url}
+      target="_blank"
+      rel="noreferrer"
+      className="py-6 md:py-0 px-0 md:px-8 lg:px-16"
+    >
+      <picture>
+        <source
+          media="(min-width:1024px)"
+          srcSet={`https://via.placeholder.com/440x290.png?text=${name.replace(
+            " ",
+            "+"
+          )}`}
+        />
+        <source
+          media="(min-width:768px)"
+          srcSet={`https://via.placeholder.com/210x140.png?text=${name.replace(
+            " ",
+            "+"
+          )}`}
+        />
+        <source
+          media="(min-width:640px)"
+          srcSet={`https://via.placeholder.com/210x120.png?text=${name.replace(
+            " ",
+            "+"
+          )}`}
+        />
+        <img
+          src={`https://via.placeholder.com/210x120.png?text=${name.replace(
+            " ",
+            "+"
+          )}`}
+          alt={name}
+        />
+      </picture>
     </a>
   );
 };
