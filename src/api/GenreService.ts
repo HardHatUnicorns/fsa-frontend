08b3fd6988afe52684038d3e70a7413a33e2f32e
jan.wielgosinski <jan.wielgosinski@th-ey.com>
2023-01-14 16:57:19 +0100

    unmock genres

---
+++ src/api/GenreService.ts
@@ -0,0 +1,7 @@
+import { Genre } from "~/models/Genre";
+import { api } from "~/AxiosApiV1";
+
+export const getGenres = async (): Promise<Genre[]> => {
+  const { data } = await api.get(`genres`);
+  return data;
+};
--- src/views/MovieCategories/AllMovieCategories.tsx
+++ src/views/MovieCategories/AllMovieCategories.tsx
@@ -1,17 +1,14 @@
 import { FC } from "react";
 import { MovieCategoryCard } from "~/views/MovieCategories/MovieCategoryCard";
+import { useQuery } from "react-query";
+import { Genre } from "~/models/Genre";
+import { getGenres } from "~/api/GenreService";
 
 export const AllMovieCategories: FC = () => {
-  const categories = [
-    { name: "Comedy", id: 1 },
-    { name: "Romance", id: 2 },
-    { name: "Horror", id: 3 },
-    { name: "Hehe", id: 4 },
-    { name: "Haha", id: 5 },
-    { name: "Thriller", id: 6 },
-    { name: "Comedy", id: 7 },
-    { name: "BlaBlaFoo extralong", id: 8 },
-  ];
+  const { data, isLoading, isFetched, isError } = useQuery<Genre[]>(
+    "all-genres",
+    getGenres
+  );
 
   return (
     <div className="w-full flex flex-col items-center">
@@ -19,9 +16,15 @@ export const AllMovieCategories: FC = () => {
         Explore movie categories
       </h1>
       <div className="max-w-7xl flex flex-row flex-wrap w-full gap-x-5 gap-y-5 justify-center align-baseline">
-        {categories.map((it) => (
-          <MovieCategoryCard key={it.id} name={it.name} />
-        ))}
+        {isLoading ? <p>Loading..</p> : null}
+        {isFetched ? (
+          <>
+            {data?.map((it) => (
+              <MovieCategoryCard key={it.id} name={it.name} />
+            ))}
+          </>
+        ) : null}
+        {isError ? <p>Could not fetch genres</p> : null}
       </div>
     </div>
   );
